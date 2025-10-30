d790d5547e85535bd88c143d8f6fdf1267702906
2400090002 <2400090002@kluniversity.in>
2025-10-30 10:29:38 +0530

    Initialize main.jsx with React and MUI setup

---
+++ main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <App />
+    </LocalizationProvider>
+  </React.StrictMode>
+);
